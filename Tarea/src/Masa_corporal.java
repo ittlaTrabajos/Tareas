5b19c9f2ff4cd7a046a072638d33bd0595eaa4c5
ittlaTrabajos <L24251117@tlalnepantla.tecnm.mx>
2024-10-29 08:49:15 -0600

    Update Masa_corporal.java

--- Tarea/src/Masa_corporal.java
+++ Tarea/src/Masa_corporal.java
@@ -5,14 +5,38 @@ public class Masa_corporal {
 	public static void main(String[] args) {
 
 		Scanner entrada=new Scanner (System.in);
+		
 		double peso,estatura;
+		boolean x;
+		
 		System.out.println("ingresa peso");
 		peso=entrada.nextDouble();
 
 		System.out.println("ingresa estatura");
 		estatura=entrada.nextDouble();
+		
 		double IMC = peso/Math.pow(estatura, 2);
-		if (IMC<=18.9)
+		
+		x=IMC<=18.9;
+		System.out.println("bajo peso "+x);
+		
+		x=IMC>=18.50 && IMC<=24.99;
+		System.out.println("peso normal "+x);
+		
+		x=IMC>=25.00 && IMC<=29.99;
+		System.out.println("sobrepeso "+x);
+		
+		x=IMC>=30.00 && IMC<=34.99;
+		System.out.println("obesidad leve "+x);
+		
+		x=IMC>=35.00 && IMC<=34.99;
+		System.out.println("obesidad media "+x);
+		
+		x=IMC>=40.00;
+		System.out.println("obesidad morbida "+x);
+		
+		
+		/*if (IMC<=18.9)
 			System.out.println("bajo peso");
 		else if(IMC>=18.50 && IMC<=24.99)
 			System.out.println("peso normal");
@@ -23,7 +47,7 @@ public class Masa_corporal {
 		else if (IMC>=35.00 && IMC<=34.99)
 			System.out.println("obesidad media");
 		else if (IMC>=40.00)
-			System.out.println("obesidad morbida");
+			System.out.println("obesidad morbida");*/
 
 			}
 
